41394b11722b83cc7b764a240a08207f5c89a54c
Quland0 <114486014+Quland0@users.noreply.github.com>
2023-09-09 03:05:53 +0300

    Add files via upload

---
+++ pz_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main (){
+  double x, y, s;
+  cout<<"введите два числа через пробел: ";
+   cin >> x >> y;
+
+  if(x>y){
+      for(int i=1; i<=20; i++)
+        s+=pow(abs(x+y), 1.0 / 3)+tan(x);
+      cout << s;
+  }
+  else if(x<y){
+      for(int i=1; i<=20; i++)
+        s = pow((y-x), 3)+cos(x);
+      cout << s;
+  }
+  else{
+      for(int i=1; i<=20; i++)
+        s+=pow((x+y), 2)+pow(x, 3);
+      cout << s;
+  }
+
+  return 0;
+}
